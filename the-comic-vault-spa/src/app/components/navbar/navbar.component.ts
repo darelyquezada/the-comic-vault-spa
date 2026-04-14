c57ac8e5d066ba0c8e63faa0f91381532a775599
Darely Quezada <darelyqg@gmail.com>
2026-04-14 14:11:46 -0600

    Adding Footer, Navbar and Header Components

--- the-comic-vault-spa/src/app/components/navbar/navbar.component.ts
+++ the-comic-vault-spa/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css',
+  styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  cartCount = this.cartService.itemCount; // Reactive cart count using Signal
+
+  constructor(public cartService: CartService) {} // Make cartService public to access itemCount signal in Template
+}
