a4915b2f461a57b172e8755c398ba3992eb8ee87
Darely Quezada <darelyqg@gmail.com>
2026-04-15 02:23:05 -0600

    Updating db.js

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,14 @@
 const mysql = require('mysql2/promise');
-const connection = mysql.createConnection({
+
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-module.exports = connection;
+module.exports = db;
